76896d635f8c0da6d0407dbdf835c7c921a175f7
Othman <120719388+Othman59@users.noreply.github.com>
2025-12-17 16:01:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Animation d'apparition des sections au scroll
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
